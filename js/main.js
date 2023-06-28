6baebbe3f29bc1c61dca2f780192a03936c315d8
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-06-28 15:13:15 -0300

    criando as funcoes para alterar os itens do robo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-const robotron = document.querySelector("#robotron")
-  robotron.addEventListener("click", dizOi);
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
-function dizOi() {
-  console.log("oi")
-}
+somar.addEventListener("click", (evento) => {
+  braco.value = parseInt(braco.value) + 1; 
+});
+
+subtrair.addEventListener("click", (evento) => {
+  braco.value = parseInt(braco.value) - 1; 
+});
+
